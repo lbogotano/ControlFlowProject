344a23e19f6cc3411f0565c06484f461179d62b7
lbogotano <62104715+lbogotano@users.noreply.github.com>
2020-05-11 07:31:39 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//First, create a simple Java project that prints "Hello World"
+
+
+class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
